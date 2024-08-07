8ceb50c4d75ab6a0ea394dafc502dbe84a98b058
Leander Ots <leander.ots@gmail.com>
2024-08-07 09:17:37 +0000

    posting ability

--- codestar/settings.py
+++ codestar/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
     'blog',
     'about',
@@ -54,6 +56,9 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -106,7 +111,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com",
     "https://*.gitpod.io",
-    "https://*.codeinstitute-ide.net"
+    "https://*.codeinstitute-ide.net",
 ]
 
 #The above trusted origins might require a connection to gitpod?
